bc723c0e326f1a83bbdcb23582a5fc46a10b807c
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-10-10 09:42:07 -0400

    THE TRENCHES ARE OVERFLOWING WITH CODE

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -1,5 +1,40 @@
-public class Main {
+import java.util.Scanner;
+import java.lang.Math;
+public class FuelCosts {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double numGallon = 0;
+        double fuelEfficiency = 0;
+        double gasPrice = 0;
+        boolean valid = false;
+        boolean trueOne = false;
+        boolean trueTwo = false;
+        boolean trueThree = false;
+
+        do {
+            System.out.println("Please enter a value for Number of Gallons.");
+
+
+            if (scan.hasNextDouble()) {
+                numGallon = scan.nextDouble();
+                scan.nextLine();
+                System.out.println("Your value for Number of Gallons is : " + numGallon);
+
+                if (numGallon >= 0);
+
+                }
+
+
+
+
+
+
+
+        } while (!valid);
+
+
+
+
     }
 }
